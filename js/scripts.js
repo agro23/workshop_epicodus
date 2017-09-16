5c26dc0dc1801e4a4f98a5087d177cbfb5dc599b
cassie_richard <cassier140@gmail.com>
2017-09-16 13:07:43 -0700

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// alert("Hello World!");
+// alert(2 + 2);
+// alert(4 * "2");
+// alert(8 + "2");
+// some_string="Hi!";
+// alert(some_string);
+// $(document).ready(function() {
+//   alert("jQuery is working");
+// })
+
+$(document).ready(function() {
+  alert($("#css-item").text());
+  alert($("#goals").text());
+})
+
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
